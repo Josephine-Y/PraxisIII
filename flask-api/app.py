15fad36f1ec4e24da8e293a6f6f558e8ccd71d7e
Obsidian <josephineyzc@gmail.com>
2026-03-27 22:11:38 -0400

    Delete duplicate mqtt-config functions

--- flask-api/app.py
+++ flask-api/app.py
@@ -187,63 +187,6 @@ def start_mqtt():
 # Start MQTT client in a separate thread
 threading.Thread(target=start_mqtt, daemon=True).start()
 
-def save_to_db(node, temp, unix_time):
-    # convert unix timestamp to datetime for Supabase
-    timestamp = datetime.fromtimestamp(unix_time, tz=timezone.utc)
-
-    try:
-        # latest_cache[node] = {"temperature": temp, "timestamp": str(timestamp)}
-
-        connection = get_db()
-        cursor = connection.cursor()
-        cursor.execute(
-            "INSERT INTO data (node, temperature, timestamp) VALUES (%s, %s, %s)",
-            (node, temp, timestamp)
-        )
-        connection.commit()
-        cursor.close()
-        connection.close()
-        print(f"Sent to database {node} = {temp}°C, {timestamp}")
-    
-    except Exception as e:
-        print(f"Receive Data Error: {e}")
-
-# MQTT Callback Methods
-# app.py
-@app.route("/mqtt-config")
-def mqtt_config():
-    return jsonify({
-        "username": os.getenv("MQTT_USERNAME"),
-        "password": os.getenv("MQTT_PASSWORD"),
-        "broker": "wss://339f0d63410548358f66c3cb882ec424.s1.eu.hivemq.cloud:8884/mqtt"
-    })
-
-def on_message(client, userdata, message):
-    topic = message.topic # nodes/{node}/jsonify(data)
-    node = topic.split("/")[1] # {node}
-    data = json.loads(message.payload.decode())
-    temp = data["temp"]
-    unix_time = data["timestamp"]
-    save_to_db(node, temp, unix_time)
-
-def on_connect(client, userdata, flags, rc):
-    client.subscribe("nodes/+/data")
-
-def start_mqtt():
-    try:
-        client = MQTT.Client()
-        client.on_connect = on_connect
-        client.on_message = on_message
-        client.username_pw_set(os.getenv("MQTT_USERNAME"), os.getenv("MQTT_PASSWORD"))
-        client.tls_set() # SSL/TLS encryption
-        client.connect("339f0d63410548358f66c3cb882ec424.s1.eu.hivemq.cloud", 8883)
-        client.loop_forever()
-    except Exception as e:
-        print(f"MQTT Client Error: {e}")
-
-# Start MQTT client in a separate thread
-threading.Thread(target=start_mqtt, daemon=True).start()
-
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     app.run(host = "0.0.0.0", port=port)
